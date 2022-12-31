d0206b9b2056cd3ab8712d4247432f6bd8e28e59
Racim Saal <racimsaal@gmail.com>
2022-12-31 02:12:23 +0100

    Main+Login Pages

---
+++ client/src/pages/Main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './main.css';
+function Main() {
+  return (
+    <div className='container'>
+      <div className='grid-item'>
+      <button>Consulter Les Actes de Naissances</button>
+      <button>Creation nouveau acte</button>
+      <button>Mise a jour nouveau acte</button>
+      </div>
+      <div className='grid-item'>
+      <button>Consulter Les Actes de Naissances</button>
+      <button>Creation nouveau acte</button>
+      <button>Mise a jour nouveau acte</button>
+      </div>
+      <div className='grid-item'> 
+      <button>Consulter Les Actes de Naissances</button>
+      <button>Creation nouveau acte</button>
+      <button>Mise a jour nouveau acte</button>
+      </div>
+    </div>
+  )
+}
+
+export default Main
