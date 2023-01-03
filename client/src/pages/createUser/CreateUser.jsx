70c3b4413ccdd034cf86e8b97c2e6c315e705670
Racim Saal <racimsaal@gmail.com>
2023-01-03 14:18:27 +0100

    Final Create User Page

--- client/src/pages/createUser/CreateUser.jsx
+++ client/src/pages/createUser/CreateUser.jsx
@@ -1,153 +1,155 @@
-import {Container, Box, Typography, TextField, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import React, { useState } from 'react'
+import {
+  Container,
+  Box,
+  Typography,
+  TextField,
+  Grid,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import React, { useState } from "react";
 
-
-function bureauService(roleValue){
-    if(roleValue === "consulaire"){
-      return(  <TextField
+function bureauService(roleValue) {
+  if (roleValue === "consulaire") {
+    return (
+      <TextField
         required
+        fullWidth
         id="pays-ratachement"
         label="Pays ratachement"
-        name='pays-ratachement'
-        /> );
-    }else{
-    return(    <TextField
-    required
-    id="bureau"
-    label="Bureau"
-    name='bureau'
-    /> );
-    }
+        name="pays-ratachement"
+      />
+    );
+  } else {
+    return (
+      <TextField required fullWidth id="bureau" label="Bureau" name="bureau" />
+    );
   }
+}
 
 function CreateUser() {
+  const [roleValue, setRoleValue] = useState(null);
+  const [dateValue, setDateValue] = useState(null);
 
-    const [roleValue, setRoleValue] = useState(null);
-    const [dateValue, setDateValue] = useState(null);
-
-    return (
-        <Container component="form" className='cadre' sx={{padding: "10px"}}>
-    
-        <Box
+  return (
+    <Container
+      component="form"
+      className="cadre"
+      sx={{ marginTop: "13%", padding: "2%" }}>
+      <Box
         component="form"
         sx={{
-          '& .MuiTextField-root': { m: 1, width: '50ch' },
+          "& .MuiTextField-root": { m: 1 },
         }}
         noValidate
-        autoComplete="off"
-      >
-        <div className='partie-auth'>
-
-        <Typography variant="h5" gutterBottom style={{marginTop:'5px'}}>
-              Creation d'un nouveau profile
+        autoComplete="off">
+        <Typography
+          variant="h5"
+          gutterBottom
+          style={{ marginTop: "5px", textAlign: "center" }}>
+          Creation d'un nouveau profile
         </Typography>
-
-          <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="matricule"
-                label="Matricule"
-                name="matricule"
-                autoComplete="matricule"
-                autoFocus
-                />
-          {/* <Grid container> */}
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            id="email"
-            label="email"
-            type="email"
-            placeholder='exemple@email.com'
+        <Grid container spacing={2} columns={16} className="partie-auth">
+          <Grid item xs={8}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="matricule"
+              label="Matricule"
+              name="matricule"
+              autoComplete="matricule"
+              autoFocus
             />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
             />
-     <FormControl margin='normal' sx={{ m:1, width: '400px' }}>
- <InputLabel id="demo-simple-select-label">Role</InputLabel>
-  <Select
-    required
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    value={roleValue}
-    label="Role"
-    onChange={(event)=> {
-      setRoleValue(event.target.value);
-    }
-  }
-  >
-    <MenuItem value={'officier'}>Officier</MenuItem>
-    <MenuItem value={'consulaire'}>Consulaire</MenuItem>
-  </Select>
-    </FormControl>
-    
-    </div>
-    <div className='partie-infogen'>
-          <Grid>  
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            id="nom"
-            label="Nom"
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="nom"
+              label="Nom"
             />
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            id="prenom"
-            label="Prenom"
-            />
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-             <DatePicker
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
                 label="Date Prise Service"
                 value={dateValue}
                 onChange={(date) => {
-                        setDateValue(date);
+                  setDateValue(date);
                 }}
-            renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        
-            {
-            roleValue ? 
-                bureauService(roleValue)
-            :
+                renderInput={(params) => <TextField fullWidth {...params} />}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={8}>
             <TextField
-            disabled
-            id="service"
-            label="Veuillez choisir le role"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="email"
+              type="email"
+              placeholder="exemple@email.com"
             />
-            }
-            </Grid>
-          
-          <Box marginBottom={10}>
-          <Button
-                type="button"
-                variant="contained"
-                style={{backgroundColor: '#00917C', float: 'right',right: '10px'}}
-                >
-                Search
-              </Button>
-                </Box>
-                </div>
-          
-    
-        </Box>
-        </Container>
-        
-        )
+            <FormControl margin="normal" sx={{ m: 1, width: "100%" }}>
+              <InputLabel id="demo-simple-select-label">Role</InputLabel>
+              <Select
+                required
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={roleValue}
+                label="Role"
+                onChange={(event) => {
+                  setRoleValue(event.target.value);
+                }}>
+                <MenuItem value={"officier"}>Officier</MenuItem>
+                <MenuItem value={"consulaire"}>Consulaire</MenuItem>
+              </Select>
+            </FormControl>
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="prenom"
+              label="Prenom"
+            />
+            {roleValue ? (
+              bureauService(roleValue)
+            ) : (
+              <TextField
+                fullWidth
+                disabled
+                id="service"
+                label="Veuillez choisir le role"
+              />
+            )}
+          </Grid>
+        </Grid>
+
+        <Button
+          fullWidth
+          type="button"
+          variant="contained"
+          style={{ backgroundColor: "#00917C", marginTop: 10 }}>
+          Create
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
-export default CreateUser
+export default CreateUser;
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,118 +1,132 @@
-import React, { useState } from 'react';
-import {Button, Grid, Avatar, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Box, Typography, Container, FormControl, InputLabel, Select, MenuItem} from '@mui/material';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
-import AccountCircleIcon  from '@mui/icons-material/AccountCircle';
-import './login.css';
+import React, { useState } from "react";
+import {
+  Button,
+  Grid,
+  Avatar,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Box,
+  Typography,
+  Container,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import "./login.css";
 
 const theme = createTheme();
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}>
+      {"Copyright © "}
       <Link color="inherit" href="https://www.interieur.gov.dz/index.php/fr/">
-      MICLAT
-      </Link>{' '}
+        MICLAT
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
-
 function Login() {
-
   const [roleValue, setRoleValue] = useState(null);
 
   return (
     <ThemeProvider theme={theme}>
-    <Container component="main" maxWidth="xs" >
-      <CssBaseline />
-      <Box
-       sx={{
-         marginTop: 8,
-         display: 'flex',
-        flexDirection: 'column',       
-        alignItems: 'center',
-      }} 
-      >
-        <Avatar  sx={{ m: 1, bgcolor: 'AppWorkspace'}}>
-          <AccountCircleIcon fontSize='large' style={{color: '#00917C'}} />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" noValidate sx={{ mt: 1,}}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="matricule"
-            label="Matricule"
-            name="matricule"
-            autoComplete="matricule"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-              <FormControl fullWidth margin='normal'>
- <InputLabel id="demo-simple-select-label">Role</InputLabel>
-  <Select
-    required
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    value={roleValue}
-    label="Role"
-    onChange={(event)=> {
-      setRoleValue(event.target.value);
-    }
-  }
-  >
-    <MenuItem value={'admin'}>Admin</MenuItem>
-    <MenuItem value={'maire'}>Maire</MenuItem>
-    <MenuItem value={'officier'}>Officier</MenuItem>
-    <MenuItem value={'consulaire'}>Consulaire</MenuItem>
-  </Select>
-    </FormControl>
-          <FormControlLabel
-            control={<Checkbox value="remember" color="success" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            style={{backgroundColor: '#00917C'}}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2" underline='none'>
-                Forgot password?
-              </Link>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}>
+          <Avatar sx={{ m: 1, bgcolor: "AppWorkspace" }}>
+            <AccountCircleIcon fontSize="large" style={{ color: "#00917C" }} />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="matricule"
+              label="Matricule"
+              name="matricule"
+              autoComplete="matricule"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControl fullWidth margin="normal">
+              <InputLabel id="demo-simple-select-label">Role</InputLabel>
+              <Select
+                required
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={roleValue}
+                label="Role"
+                onChange={(event) => {
+                  setRoleValue(event.target.value);
+                }}>
+                <MenuItem value={"admin"}>Admin</MenuItem>
+                <MenuItem value={"maire"}>Maire</MenuItem>
+                <MenuItem value={"officier"}>Officier</MenuItem>
+                <MenuItem value={"consulaire"}>Consulaire</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="success" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{ backgroundColor: "#00917C" }}>
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2" underline="none">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2" underline="none">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="#" variant="body2" underline='none'>
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-      <Copyright sx={{ mt: 8, mb: 4 }} />
-    </Container>
- </ThemeProvider>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
 }
 
-export default Login
+export default Login;
